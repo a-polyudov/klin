11544b8dd1b5f334f591ee167b21ba7e6950c600
Alexander Polyudov <a.poludov@gmail.com>
2023-02-04 22:57:37 +0600

    Bump dependencies versions (#19)
    Bump dependencies versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,7 @@
-import org.jetbrains.kotlin.incremental.cleanDirectoryContents
-
 plugins {
-  kotlin("js") version "1.7.10"
-  kotlin("plugin.serialization") version "1.7.10"
-  id("io.gitlab.arturbosch.detekt") version "1.21.0"
+  kotlin("js") version "1.8.10"
+  kotlin("plugin.serialization") version "1.8.10"
+  id("io.gitlab.arturbosch.detekt") version "1.22.0"
 }
 
 group = "tech.poludov"
@@ -17,17 +15,20 @@ repositories {
 dependencies {
   testImplementation(kotlin("test"))
   implementation("org.jetbrains.kotlinx:kotlinx-html-js:0.8.0")
-  implementation("org.jetbrains.kotlin:kotlin-stdlib-js:1.7.10")
+  implementation("org.jetbrains.kotlin:kotlin-stdlib-js:1.8.10")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-  implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
+  implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
 }
+
 
 kotlin {
   js(IR) {
     binaries.executable()
     browser {
       commonWebpackConfig {
-        cssSupport.enabled = true
+        cssSupport {
+          enabled.set(true)
+        }
         outputFileName = "popup.js"
       }
     }
@@ -51,8 +52,8 @@ tasks {
     group = "build"
     dependsOn("detekt", "assemble", "test")
     doLast {
-      val releaseDir = File(releaseDirPath).apply {
-        cleanDirectoryContents()
+      val releaseDir = File(releaseDirPath).also {
+        delete(it)
       }
       File("${buildDir.toPath()}/distributions").copyRecursively(releaseDir)
     }
